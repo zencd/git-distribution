a256547fb15e3fafb92ca7d1a947b2d6c2946c81
zencd <zd@z.com>
2021-01-09 23:56:10 +0300

    wip

--- app/tools/update.py
+++ app/tools/update.py
@@ -50,24 +50,47 @@ def get_local_and_remote_shas(local_repo):
     return local_sha, remote_sha
 
 
+'''
+def load_remote_history():
+    url = 'https://raw.githubusercontent.com/zencd/git-distribution/release/history.txt'
+    resp = requests.get(url)
+    if not resp.ok:
+        return []
+    return [line.strip() for line in resp.text.splitlines()]
+'''
+
+
+def load_local_history(history_file):
+    if not os.path.exists(history_file):
+        return []
+    with open(history_file, 'r') as fd:
+        return [line.strip() for line in fd]
+
+
 def main():
     git_url = 'https://github.com/zencd/git-distribution'
     git_branch = 'release'
     app_dir = str(Path(__file__).parent.parent)
     repo_dir = os.path.join(app_dir, 'repo')
     repo_dir_tmp = os.path.join(app_dir, 'repo_')
+    local_history_file = os.path.join(repo_dir, 'history.txt')
 
     print(f'App dir: {app_dir}')
     print(f'Updating from {git_url} branch {git_branch}')
 
+    history_before = load_local_history(local_history_file)
+
     repo = None
 
     if os.path.exists(repo_dir):
-        repo = pygit2.Repository(repo_dir)
-        local_sha, remote_sha = get_local_and_remote_shas(repo)
-        if (local_sha == remote_sha) and (local_sha is not None) and (remote_sha is not None):
-            print(f'Already up to date')
-            return
+        try:
+            repo = pygit2.Repository(repo_dir)
+            local_sha, remote_sha = get_local_and_remote_shas(repo)
+            if (local_sha == remote_sha) and (local_sha is not None) and (remote_sha is not None):
+                print(f'Already up to date')
+                return
+        except Exception as e:
+            print(f'Error: {e}')
 
     rmtree(repo_dir_tmp)
     git_clone(git_url, repo_dir_tmp, git_branch)
@@ -75,6 +98,13 @@ def main():
     shutil.copytree(repo_dir_tmp, repo_dir)
     rmtree(repo_dir_tmp)
     print('Updated to the recent version')
+
+    history_after = load_local_history(local_history_file)
+    diff = [x for x in history_after if x and x not in history_before]
+    if diff:
+        print('What is new:')
+        for line in diff:
+            print(line)
 
 
 if __name__ == '__main__':
