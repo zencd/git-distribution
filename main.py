a8fcc2f6febb687f83328505acdba4f8c0aa2b71
zencd <zd@z.com>
2021-01-16 05:14:56 +0300

    wip

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pygit2
 
+'''
+def load_remote_history():
+    url = 'https://raw.githubusercontent.com/zencd/git-distribution/release/history.txt'
+    resp = requests.get(url)
+    if not resp.ok:
+        return []
+    return [line.strip() for line in resp.text.splitlines()]
+'''
+
+
 if __name__ == '__main__':
     print('hello from the app v3')
