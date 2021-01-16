46b3c6a6d1df9c57f66feec6af5b4418fd4b94ff
zencd <zd@z.com>
2021-01-16 20:16:22 +0300

    wip

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import pygit2
 import sys
 
 if __name__ == '__main__':
-    print('hello from the app v3')
-    print(f'args: {sys.argv}')
+    print('hello from the app v4')
+    # print(f'args: {sys.argv}')
